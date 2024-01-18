9a0944c7c42299fc592b10fb14d4bf8a997c2eb7
tamatokkyuu68 <149918362+tamatokkyuu68@users.noreply.github.com>
2024-01-18 21:50:09 +0900

    kadai_005をアップロード

---
+++ html/score.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 変数を作成し、値を代入
+        $scores = [80,60,55,40,100,25,80,95,30,60];
+
+        // 合計点
+        echo 80+60+55+40+100+25+80+95+30+60;
+
+        echo "<br>";
+
+        // 平均点
+        echo 625 / 10;
+        ?>
+    </p>
+</body>

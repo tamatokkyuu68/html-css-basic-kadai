440be7a6d4e448bd9d033c0ab1d4c8d3a8c64cda
tamatokkyuu68 <149918362+tamatokkyuu68@users.noreply.github.com>
2024-01-21 14:21:37 +0900

    kadai_007

---
+++ html/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <neta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vegetable = ['name' => 'onion','price' => 200,'weight' => 160];
+
+        print_r($vegetable);
+        ?>
+    </p>
+</body>

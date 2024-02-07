2e0bdff53066950c85168ec693de729c63d38296
tamatokkyuu68 <149918362+tamatokkyuu68@users.noreply.github.com>
2024-02-07 17:56:01 +0900

    kadai_016

---
+++ html/class.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta chareset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <?php
+    class Food{
+        private $name;
+        private $price;
+
+        public function show_price(){
+            echo $this->price . '<br>';
+        }
+
+        public function __construct(string $name, int $price){
+            $this->name = $name;
+            $this->price = $price;
+        }
+
+    }
+
+    class Animal{
+        private $name;
+        private $height;
+        private $weight;
+
+        public function show_height(){
+            echo $this->height . '<br>';
+        }
+
+        public function __construct(string $name, int $height, int $weight){
+            $this->name = $name;
+            $this->height = $height;
+            $this->weight = $weight;
+        }
+
+    }
+
+    $food = new Food('potato',250);
+    print_r($food);
+
+    $animal = new Animal('dog',60,5000);
+    print_r($animal);
+    
+
+    $food->show_price(); 
+    $animal->show_height();
+    
+    ?>
+</body>
+
+</html>
